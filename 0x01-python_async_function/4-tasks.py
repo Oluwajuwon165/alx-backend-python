4420f68f1a7f1a0ac2acce6d5613a82f4c2b3fad
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-05-02 20:52:59 +0100

    Altering wait_n into a new function task_wait_n

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,8 +9,8 @@ task_wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Wait for a random delay between 0 and max_delay seconds and return
-    a list of them.
+    """Wait for a random delay between 0 and max_delay seconds
+    and return a list of them.
     """
     tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
