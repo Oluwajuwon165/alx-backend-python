3e57a3636056c114d6311283c51bf63856c52293
Oluwajuwon165 <99687642+Oluwajuwon165@users.noreply.github.com>
2023-05-03 19:18:40 +0100

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """Asynchronous generator that yields random numbers."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.uniform() * 10
