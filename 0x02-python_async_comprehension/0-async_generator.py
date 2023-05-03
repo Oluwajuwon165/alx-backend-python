2d59a910900cfd9af45e45ab2656df1cfd57bc4d
Oluwajuwon165 <99687642+Oluwajuwon165@users.noreply.github.com>
2023-05-03 19:04:13 +0100

    A coroutine that takes no arguments

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-"""
-Write a coroutine called async_generator that takes no arguments.
-The coroutine will loop 10 times, each time asynchronously wait 1 second,
-then yield a random number between 0 and 10. Use the random module.
-"""
+"""Module with an asynchronous generator that yields random numbers."""
 
 import asyncio
 import random
@@ -11,6 +7,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Loop 10 times, wait 1 sec each time"""
-    await asyncio.sleep(0)  # Allow event loop to switch tasks
-    return (random.uniform(0, 10) for _ in range(10))
+    """Asynchronous generator that yields random numbers."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
