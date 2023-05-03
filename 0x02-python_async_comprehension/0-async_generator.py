c3fc40a3b7a1bd086d2fd4aa87de89cbc929c309
Oluwajuwon165 <99687642+Oluwajuwon165@users.noreply.github.com>
2023-05-03 19:26:19 +0100

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Asynchronous generator that yields random numbers."""
     for Jhay in range(10):
         await asyncio.sleep(1)
