45b12f57f163b82d3ca62e680ffc80a89ec2dbc9
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-05-22 16:21:27 +0100

    Task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,12 +7,12 @@ from unittest.mock import patch
 from utils import memoize
 from unittest.mock import patch, Mock
 
-        
+
 class TestMemoize(unittest.TestCase):
     """
     TestMemoize class
     """
-        
+
     def test_memoize(self):
         """
         Test the memoize decorator
@@ -28,7 +28,8 @@ class TestMemoize(unittest.TestCase):
         test_instance = TestClass()
 
         with patch.object(test_instance, 'a_method') as mock_a_method:
-            mock_a_method.return_value = Mock(return_value=42)  # Set the return value of the mock
+            # Set the return value of the mock
+            mock_a_method.return_value = Mock(return_value=42)
 
             result_1 = test_instance.a_property()
             result_2 = test_instance.a_property()
